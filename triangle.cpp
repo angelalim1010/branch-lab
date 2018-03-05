513217d5e8e8e18ca1523bc706578a279b2bf5b8
Angela Lim <angela.lim57@myhunter.cuny.edu>
2018-03-05 10:25:57 -0500

    triangle

---
+++ triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "doctest.h"
+
+#include "triangle.h"
+
+
+using std::string;
+
+string triangle(int height){
+  string s = "";
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j < width; j++) {
+      s=s+"*";
+    }
+    s=s+"\n";
+  }
+  return s;
+}
+
+TEST_CASE("Testing triangle"){
+  string s = triangle(4);
+  CHECK(s=="*\n**\n***\n****");
+}
---
+++ triangle.h
@@ -0,0 +1,9 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <iostream>
+using std::string;
+string box(int width, int height);
+
+
+#endif
